3bff86354d452e4a36aa363ab00b21f6e191e6e7
Ulil Albab <ulil.alb21@gmail.com>
2024-11-16 16:28:55 +0700

    setup routes and init components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
+  {
+    path: 'users',
+    component: UserListComponent,
+  },
+  {
+    path: 'users/:userId',
+    component: UserDetailsComponent,
+  },
+];
